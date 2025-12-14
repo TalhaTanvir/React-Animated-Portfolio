e46775bd76b1fea6786e4a36a150f406373780e1
Talha <talhatanveer202@gmail.com>
2025-12-14 23:55:19 +0500

    new changes in experiences section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,5 +1,5 @@
-import { useTransform } from 'motion/react';
-import React from 'react'
+import {motion, useScroll, useTransform } from 'framer-motion';
+import React, { useEffect, useState, useRef, useMemo } from 'react'
 
 
 
@@ -38,19 +38,75 @@ function ExperienceItem({exp, idx, start, end, scrollYprogress, layout}){
         >
 
         </motion.div>
-        <motion.article className>
+        <motion.article className={`absolute ${idx%2===0 ? "bottom-12" : "top-12"} bg-gray-900/80 backdrop-blur border border-gray-700/70 rounded-xl p-7 w-[320px] shadow-lg`}
+        style={{opacity , y , maxWidth : "90vw"}}
+        transition = {{duration : 0.4 , delay : idx*0.15}}
+        >
+          <h3 className='text-xl font-semibold'>
+            {exp.role}
+          </h3>
+          <p className='text-md text-gray-400 mb-3'>
+            {exp.company} | {exp.duration}
+          </p>
+          <p className='text-md text-gray-300 break-words'>
+            {exp.description}
+          </p>
 
         </motion.article>
 
       </div>
     )
   }
+  return(
+    <div className='relative flex items-start'>
+      <motion.div className="absolute -left-[14px] top-3 z-10 w-7 h-7 rounded-full bg-white shadow-[0_0_0_8px_rgba(255,255,255,0.1)]"
+      style={{scale , opacity}}
+      >
+
+      </motion.div>
+      <motion.article className="bg-gray-900/80 backdrop-blur border border-gray-700/70 rounded-xl p-5 w-[90vw] max-w-sm ml-6 shadow-lg"
+      style={{opacity , x}}
+      transition={{duration : 0.4 , delay: idx*0.15}}
+      >
+        <h3 className='text-lg font-semibold break-words'>
+          {exp.role}
+        </h3>
+        <p className='text-sm text-gray-400 mb-2 break-words'>
+          {exp.company} | {exp.duration}
+        </p>
+        <p className='text-sm text-gray-300 break-words'>
+          {exp.description}
+        </p>
+
+      </motion.article>
+    </div>
+  )
 }
 
 function Experience() {
+
+  const sceneRef = useRef(null);
+  const [isMobile , setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => setIsMobile(window.innerWidth < 768);
+    checkMobile();
+    window.addEventListener("resize" , checkMobile)
+    return () => window.removeEventListener("resize" , checkMobile)
+  },[])
+
+  const SCENE_HEIGHT_VH = isMobile ? 160*experiences.length : 120*experiences.length;
+
+  const {scrollYprogress} = useScroll({
+    target: sceneRef,
+    offset:["start start" , "end end"]
+  })
+
+  const thresholds = useMemo(() => experiences.map((_, i) => (i+1) / experiences.length),[])
+  const lineSize = useTransform(scrollYprogress , (v) => `${v*100}%`)
   return (
     <section id='experience' className='relative bg-black text-white'>
-
+      section
     </section>
   )
 }
