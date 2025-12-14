2bf1e77a8952c186d80a44a323a78624f0c6a2b6
Talha <talhatanveer202@gmail.com>
2025-12-15 01:56:59 +0500

    New Changes

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -34,7 +34,7 @@ function ExperienceItem({exp, idx, start, end, scrollYprogress, layout}){
     return(
       <div className='relative flex flex-1 justify-center items-center min-w-0'>
         <motion.div className="z-10 w-7 h-7 rounded-full bg-white shadow-[0_0_0_8px_rgba(255,255,255,0.1)]"
-        style={{height: 40 , opacity}}
+        style={{opacity}} //height:40 remove inside it
         >
 
         </motion.div>
@@ -45,10 +45,10 @@ function ExperienceItem({exp, idx, start, end, scrollYprogress, layout}){
           <h3 className='text-xl font-semibold'>
             {exp.role}
           </h3>
-          <p className='text-md text-gray-400 mb-3'>
+          <p className='text-base text-gray-400 mb-3'>
             {exp.company} | {exp.duration}
           </p>
-          <p className='text-md text-gray-300 break-words'>
+          <p className='text-base text-gray-300 break-words'>
             {exp.description}
           </p>
 
@@ -97,16 +97,84 @@ function Experience() {
 
   const SCENE_HEIGHT_VH = isMobile ? 160*experiences.length : 120*experiences.length;
 
-  const {scrollYprogress} = useScroll({
+  const {scrollYProgress} = useScroll({
     target: sceneRef,
     offset:["start start" , "end end"]
   })
 
   const thresholds = useMemo(() => experiences.map((_, i) => (i+1) / experiences.length),[])
-  const lineSize = useTransform(scrollYprogress , (v) => `${v*100}%`)
+  const lineSize = useTransform(scrollYProgress , (v) => `${v*100}%`)
+
   return (
     <section id='experience' className='relative bg-black text-white'>
-      section
+      <div ref={sceneRef}
+      style={{height: `${SCENE_HEIGHT_VH}vh` , minHeight: "120vh"}}
+      className='relative'
+      >
+        <div className='sticky top-0 h-screen flex flex-col '>
+          <h2 className='text-4xl sm:text-5xl font-semibold mt-5 text-center'>
+            Experience
+          </h2>
+          <div className='flex flex-1 items-center justify-center px-6 pb-10'>
+            {!isMobile && (
+              <div className='relative w-full max-w-7xl'>
+                <div className='relative h-[6px] bg-white/15 rounded'>
+                  <motion.div className='absolute left-0 top-0 h-[6px] bg-white rounded origin-left'
+                  style={{width : lineSize}}
+                  >
+
+                  </motion.div>
+                </div>
+
+                <div className='relative flex justify-between mt-0'>
+                  {experiences.map((exp , idx) => (
+                    <ExperienceItem
+                    key={idx}
+                    exp={exp}
+                    idx={idx}
+                    start={idx === 0 ? 0 : thresholds[idx-1]}
+                    end={thresholds[idx]}
+                    scrollYprogress={scrollYProgress}
+                    layout="desktop"
+                    />
+                  ))}
+
+                </div>
+
+              </div>
+            )}
+
+            {isMobile && (
+              <div className='relative w-full max-w-md'>
+                <div className='absolute left-0 top-0 bottom-0 w-[6px] bg-white/15 rounded'>
+                  <motion.div className='absolute top-0 left-0 w-[6px] bg-white rounded origin-top'
+                  style={{height : lineSize}}
+                  >
+
+                  </motion.div>
+                </div>
+
+                <div className='relative flex flex-col gap-10 ml-10 mt-6 pb-28 '>
+                  {experiences.map((exp,idx) => (
+                    <ExperienceItem
+                    key={idx}
+                    exp={exp}
+                    idx={idx}
+                    start={idx === 0 ? 0 : thresholds[idx-1]}
+                    end={thresholds[idx]}
+                    scrollYprogress={scrollYProgress}
+                    layout="mobile"
+                    />
+                  ))}
+
+                </div>
+              </div>
+            )}
+          </div>
+
+        </div>
+
+      </div>
     </section>
   )
 }
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { FaGithub, FaLinkedin, FaXTwitter } from 'react-icons/fa6'
+import {motion} from 'framer-motion'
+
+
+
+const socials = [
+  {Icon: FaXTwitter , label: "X" , href: "https://twitter.com"},
+  {Icon: FaLinkedin , label: "LinkedIn" , href: "https://linkedin.com"},
+  {Icon: FaGithub , label: "GitHub" , href: "https://github.com"},
+]
+
+const glowVarients = {
+  initial : {scale:1 , y:0, filter:"drop-shadow(0 0 0 rgba(0,0,0))"},
+  hover : {
+    scale: 1.2 , y: -3,
+    filter: "drop-shadow(0 0 8px rgba(13,88,204,0.9)) drop-shadow(0 0 18px rgba(16,185,129,0.8))",
+    transition: {type: "spring", stifness:300, damping:15}
+  },
+  tap: {scale:0.95, y:0, transition:{duration:0.88}}
+}
+
+
 
 function Footer() {
   return (
-    <div className='w-full h-screen'>Footer</div>
+    <section className='w-full h-screen'>
+      <motion.div className='relative z-10 px-4 sm:px-8 lg:px-10 py-16 md:py-20 flex flex-col items-center text-center space-y-6'
+      initial={{opacity: 0 , y: 30}}
+      whileInView={{opacity:1 , y:0}}
+      transition={{duration:0.8}}
+      >
+        <h1 className='font-semibold leading-none text-white texxt-center select-none'
+        style={{
+          fontSize: "clamp(3rem,5vw,14rem)",
+          letterSpacing: "0.02em",
+          lineHeight: 0.9,
+          padding:"0 3vw",
+          whiteSpace: "nowrap",
+          textShadow: "0 2px 18px rgba(0,0,0,0)"
+
+        }}
+        >
+          Talha Tanvir
+        </h1>
+
+      </motion.div>
+    </section>
   )
 }
 
